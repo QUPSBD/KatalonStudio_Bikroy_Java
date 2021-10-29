0bca3fdf1e1aa83780d486aad48ffc007efb6967
asif-gamespace <unidevgo.qa1@gmail.com>
2021-10-29 23:34:01 +0600

    Bikroy.com automation sample with katalon studio

---
+++ Test Suites/TestSuite2.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>This is others testcases</description>
+   <name>TestSuite2</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>2b061ebb-edc5-4854-baa7-22b901f66b33</testSuiteGuid>
+   <testCaseLink>
+      <guid>a2b11e72-a7a0-4e68-b423-8e61c27769b1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SearchTest</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
